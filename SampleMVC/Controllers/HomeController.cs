1f01aaf6cbbed1ac8f5920e2f70e68ceee7abdce
sordahl-ga <sordahl@bellsouth.net>
2018-12-11 13:19:03 -0500

    Initial

--- SampleMVC/Controllers/HomeController.cs
+++ SampleMVC/Controllers/HomeController.cs
@@ -1,9 +1,12 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.IO;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
-
+using Microsoft.Azure; // Namespace for Azure Configuration Manager
+using Microsoft.WindowsAzure.Storage; // Namespace for Storage Client Library
+using Microsoft.WindowsAzure.Storage.File; // Namespace for Azure Files
 namespace SampleMVC.Controllers
 {
     public class HomeController : Controller
@@ -26,5 +29,66 @@ namespace SampleMVC.Controllers
 
             return View();
         }
+        private byte[] ReadFully(Stream input)
+        {
+            byte[] buffer = new byte[16 * 1024];
+
+            input.Position = 0; // Add this line to set the input stream position to 0
+
+            using (MemoryStream ms = new MemoryStream())
+            {
+                int read;
+                while ((read = input.Read(buffer, 0, buffer.Length)) > 0)
+                {
+                    ms.Write(buffer, 0, read);
+                }
+                return ms.ToArray();
+            }
+        }
+        public ActionResult DownloadFile(string filename)
+        {
+            // Parse the connection string and return a reference to the storage account.
+            CloudStorageAccount storageAccount = CloudStorageAccount.Parse(
+                CloudConfigurationManager.GetSetting("StorageConnectionString"));
+            // Create a CloudFileClient object for credentialed access to Azure Files.
+            CloudFileClient fileClient = storageAccount.CreateCloudFileClient();
+
+            // Get a reference to the file share we created previously.
+            CloudFileShare share = fileClient.GetShareReference("stuff");
+
+            // Ensure that the share exists.
+            if (share.Exists())
+            {
+                // Get a reference to the root directory for the share.
+                CloudFileDirectory rootDir = share.GetRootDirectoryReference();
+
+                // Get a reference to the directory we created previously.
+                CloudFileDirectory filesdir = rootDir.GetDirectoryReference("files");
+
+                // Ensure that the directory exists.
+                if (filesdir.Exists())
+                {
+                    // Get a reference to the file we created previously.
+                    CloudFile file = filesdir.GetFileReference(filename);
+
+                    // Ensure that the file exists.
+                    if (file.Exists())
+                    {
+                        byte[] content = ReadFully(file.OpenRead());
+                        FileContentResult result = new FileContentResult(content, "application/pdf")
+                        {
+                            FileDownloadName = filename
+                        };
+
+                        return result;
+                    }
+                }
+            }
+            return new HttpNotFoundResult("File not found " + filename);
+        }
     }
 }
+
+
+
+        
